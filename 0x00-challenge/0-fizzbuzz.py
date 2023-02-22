6ffd72e833eed7e9c8396d5092ba14e456a4ce6a
BusolaAM <busolaatinsola@gmail.com>
2023-02-22 23:36:44 +0100

    0. FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -4,7 +4,6 @@
 """
 import sys
 
-
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -27,7 +26,6 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
-
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
